60fe089a04bf638480ffbe50c2cceaffbb8aeb80
Daniel A <DanielAAad01@gmail.com>
2023-12-30 10:05:24 -0800

    Added basic styling of modal window.

--- src/modular.js
+++ src/modular.js
@@ -1,3 +1,19 @@
 import { currentTimezone } from './clock.js';
+import MicroModal from 'micromodal';
 
 console.log(currentTimezone);
+
+MicroModal.init({
+    onShow: modal => {
+        console.info(`${modal.id} is shown`);
+        document.querySelector('#timezone-modal').classList.remove('hidden');
+    },
+    onClose: modal => {
+        console.info(`${modal.id} is hidden`);
+        document.querySelector('#timezone-modal').classList.add('hidden');
+    }
+});
+
+// For every available time zone, create an option within #timezone-select
+
+const timezoneArr = [];
